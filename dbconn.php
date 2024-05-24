a3fd2e8ed7f44e8917c0fd9239bfc03a0fdd408d
meDigital Development <medigital.net@gmail.com>
2024-05-24 20:07:21 +0700

    Upgrade v2024

---
+++ dbconn.php
@@ -0,0 +1,9 @@
+<?php
+date_default_timezone_set('Asia/Jakarta');
+
+$server   = "localhost";
+$user     = "root";
+$pass     = "";
+$database = "ppdb";
+
+$conn = mysqli_connect($server, $user, $pass, $database);
