6ded0b48eb352547fd7ce342ee6cb466c89b2b41
meDigital Development <medigital.net@gmail.com>
2025-03-18 11:38:08 +0700

    Update widget.php

--- api/widget.php
+++ api/widget.php
@@ -12,11 +12,11 @@ requireLogin();
 
 $response = [
     'info' => count(query("SELECT * FROM informasi")),
-    'berkas' => count(query("SELECT * FROM berkas")) . '/' . count(query("SELECT * FROM berkas WHERE status = 1")),
+    'berkas' => count(query("SELECT * FROM berkas")),
     'banner' => count(query("SELECT * FROM banner")),
     'event' => count(query("SELECT * FROM event")),
-    'tautan' => count(query("SELECT * FROM tautan")) . '/' . count(query("SELECT * FROM tautan WHERE aktif = 1")),
-    'forum' => count(query("SELECT * FROM forum")) . '/' . count(query("SELECT * FROM forum WHERE dibaca = 0")),
+    'tautan' => count(query("SELECT * FROM tautan")),
+    'forum' => count(query("SELECT * FROM forum")),
 ];
 
 echo json_encode($response, JSON_PRETTY_PRINT);
