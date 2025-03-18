851c88bb9381448b2b95d32f61c05e339e0d32ab
meDigital Development <medigital.net@gmail.com>
2025-03-18 08:48:14 +0700

    Fix: Theme color mode

--- assets/js/color-modes.js
+++ assets/js/color-modes.js
@@ -1,3 +1,9 @@
+/*!
+ * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
+ * Copyright 2011-2023 The Bootstrap Authors
+ * Licensed under the Creative Commons Attribution 3.0 Unported License.
+ */
+
 (() => {
   "use strict";
 
@@ -9,22 +15,26 @@
     if (storedTheme) {
       return storedTheme;
     }
+
     return window.matchMedia("(prefers-color-scheme: dark)").matches
       ? "dark"
       : "light";
   };
 
   const setTheme = (theme) => {
-    if (
-      theme === "auto" &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-    ) {
-      document.documentElement.setAttribute("data-bs-theme", "dark");
-      setLogo("dark");
+    let activeTheme;
+    if (theme === "auto") {
+      const currentTheme = window.matchMedia("(prefers-color-scheme: dark)")
+        .matches
+        ? "dark"
+        : "light";
+      document.documentElement.setAttribute("data-bs-theme", currentTheme);
+      activeTheme = currentTheme;
     } else {
       document.documentElement.setAttribute("data-bs-theme", theme);
-      setLogo(theme);
+      activeTheme = theme;
     }
+    setLogo(activeTheme);
   };
 
   const setLogo = (theme) => {
@@ -45,6 +55,45 @@
 
   setTheme(getPreferredTheme());
 
+  const showActiveTheme = (theme, focus = false) => {
+    const themeSwitcher = document.querySelector("#bd-theme");
+    if (!themeSwitcher) return;
+
+    const themeSwitcherText = document.querySelector("#bd-theme-text");
+    const activeThemeIcon = document.querySelector(".theme-icon-active use");
+    const btnToActive = document.querySelector(
+      `[data-bs-theme-value="${theme}"]`
+    );
+    const svgOfActiveBtn = btnToActive
+      .querySelector("svg use")
+      .getAttribute("href");
+
+    // Hapus class "active" dari semua tombol tema
+    document.querySelectorAll("[data-bs-theme-value]").forEach((element) => {
+      element.classList.remove("active");
+      element.setAttribute("aria-pressed", "false");
+    });
+
+    // Tambahkan class "active" ke tombol tema yang dipilih
+    btnToActive.classList.add("active");
+    btnToActive.setAttribute("aria-pressed", "true");
+
+    // Ubah ikon tema utama di tombol dropdown
+    activeThemeIcon.setAttribute("href", svgOfActiveBtn);
+
+    // Perbarui label toggle
+    const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`;
+    themeSwitcher.setAttribute("aria-label", themeSwitcherLabel);
+
+    // Menampilkan checkmark di tema aktif
+    document.querySelectorAll(".dropdown-item svg.ms-auto").forEach((icon) => {
+      icon.classList.add("d-none");
+    });
+    btnToActive.querySelector("svg.ms-auto").classList.remove("d-none");
+
+    if (focus) themeSwitcher.focus();
+  };
+
   window
     .matchMedia("(prefers-color-scheme: dark)")
     .addEventListener("change", () => {
@@ -55,12 +104,14 @@
     });
 
   window.addEventListener("DOMContentLoaded", () => {
-    setLogo(getPreferredTheme());
+    showActiveTheme(getPreferredTheme());
+
     document.querySelectorAll("[data-bs-theme-value]").forEach((toggle) => {
       toggle.addEventListener("click", () => {
         const theme = toggle.getAttribute("data-bs-theme-value");
         setStoredTheme(theme);
         setTheme(theme);
+        showActiveTheme(theme, true);
       });
     });
   });
