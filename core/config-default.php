eda891279ac1cfe2a828c9bc38463d491f428c0c
meDigital Development <medigital.net@gmail.com>
2025-03-31 15:49:57 +0700

    Create config-default.php

---
+++ core/config-default.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * environment
+ * development|production|testing
+ */
+define('ENV', 'development');
+
+/**
+ * Set timezone
+ */
+date_default_timezone_set('Asia/Jakarta');
+
+/**
+ * API_KEY
+ */
+define('API_KEY', 'muhsaidlg');
+
+/**
+ * Database config
+ */
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'ppdb');
+
+/**
+ * Constanta
+ */
+define('ROOT', dirname(__DIR__));
