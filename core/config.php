c21723840f870f19ef71f646f0dd7d9098dbc13e
meDigital Development <medigital.net@gmail.com>
2025-04-26 19:31:07 +0700

    Update

--- core/config.php
+++ core/config.php
@@ -12,19 +12,9 @@ define('ENV', 'development');
 date_default_timezone_set('Asia/Jakarta');
 
 /**
- * API_KEY
- */
-define('API_KEY', 'muhsaidlg');
-
-/**
  * Database config
  */
 define('DB_SERVER', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'ppdb');
-
-/**
- * Constanta
- */
-define('ROOT', dirname(__DIR__));
