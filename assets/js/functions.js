2dc2418a08bf0624a446f9ed059197657f614264
meDigital Development <medigital.net@gmail.com>
2025-05-15 08:18:27 +0700

    Add: Error Handler

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -385,3 +385,19 @@ async function counterInner() {
     }
   }
 }
+
+function errorHandle(err) {
+  if (err.responseJSON && err.responseJSON.message) {
+    toast(err.responseJSON.message, "error", 0);
+  } else if (err.code === 400) {
+    toast(err.messages, "error", 0);
+  } else if (err.code === 500 || err.status === 400) {
+    toast(
+      err.responseJSON ? err.responseJSON.messages.error : "Error server",
+      "error",
+      0
+    );
+  } else {
+    toast("An error occurred", "error", 0);
+  }
+}
